6a2635bf7d2ea75de24d81ba287dd97fcf007a3a
subhrachk <subhranil.chakraborty@hotmail.com>
2024-03-19 16:56:36 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { EventsComponent } from './components/events/events.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    EventsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
